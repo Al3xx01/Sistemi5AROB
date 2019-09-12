6ff697d1f4a228d8bd560cac42b7da5bf9b8625d
Al3xx01 <46714833+Al3xx01@users.noreply.github.com>
2019-09-12 09:06:36 +0200

    Add files via upload

--- sistemi/ServerClientTCP/client.py
+++ sistemi/ServerClientTCP/client.py
@@ -1,7 +1,28 @@
 import socket
+import re
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 1025))
+while True:
+    IP_SERVER = input("IP server: ")
+    pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    test = pat.match(IP_SERVER)
+    if test:
+        while True:
+            PORT = input("PORT server: ")
+            pat = re.compile("^()([1-9]|[1-5]?[0-9]{2,4}|6[1-4][0-9]{3}|65[1-4][0-9]{2}|655[1-2][0-9]|6553[1-5])$")
+            test = pat.match(PORT)
+            PORT = int(PORT)
+            if test:
+                break
+            else:
+                print("\nPORTA NON CORRETTA\n")
+        break
+    else:
+        print("\nIP NON CORRETTO\n")
+    
+
+
+s.connect((IP_SERVER, PORT))
 while True:
     text = input("Messaggio: ")
 
@@ -14,5 +35,4 @@ while True:
         data = s.recv(4096)
         print("Server: " + data.decode())
 
-conn.close()
 s.close()
